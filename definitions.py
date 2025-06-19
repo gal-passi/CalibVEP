df450fb0aa11ca1ded962d090ff5b5d53cc621c1
Gal Passi <galpassi+1@gmail.com>
2025-06-19 12:11:50 +0300

    Update definitions.py

--- definitions.py
+++ definitions.py
@@ -3,5 +3,13 @@ import torch
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 #  VARIANT PROCESSING 
+
 VALID_AA = "ACDEFGHIKLMNPQRSTVWY"
+N_AA = len(VALID_AA)
+AA_SYN = {"A": "ALA", "C": "CYS", "D": "ASP", "E": "GLU", "F": "PHE", "G": "GLY", "H": "HIS", "I": "ILE",
+          "K": "LYS", "L": "LEU", "M": "MET", "N": "ASN", "P": "PRO", "Q": "GLN", "R": "ARG", "S": "SER",
+          "T": "THR", "V": "VAL", "W": "TRP", "Y": "TYR"}
+AA_SYN_REV = dict((v, k) for k, v in AA_SYN.items())
+AA_TO_INDEX_ESM = {'K': 0, 'R': 1, 'H': 2, 'E': 3, 'D': 4, 'N': 5, 'Q': 6, 'T': 7, 'S': 8, 'C': 9, 'G': 10,
+                   'A': 11, 'V': 12, 'L': 13, 'I': 14, 'M': 15, 'P': 16, 'Y': 17, 'F': 18, 'W': 19}
 MUTATION_REGEX =  rf'p\.(?P<symbol>(?P<orig>[{VALID_AA}]){{1}}(?P<location>[\d]+)(?P<change>[{VALID_AA}]){{1}})'
