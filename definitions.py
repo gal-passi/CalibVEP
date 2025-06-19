92f9623f4d0e9a40413feada0ecf73b2ab119bff
Gal Passi <galpassi+1@gmail.com>
2025-06-19 12:10:43 +0300

    Update definitions.py

--- definitions.py
+++ definitions.py
@@ -1,3 +1,7 @@
 import torch
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+#  VARIANT PROCESSING 
+VALID_AA = "ACDEFGHIKLMNPQRSTVWY"
+MUTATION_REGEX =  rf'p\.(?P<symbol>(?P<orig>[{VALID_AA}]){{1}}(?P<location>[\d]+)(?P<change>[{VALID_AA}]){{1}})'
