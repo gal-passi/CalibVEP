656473c66495bc97f41f5d07c807ec300d64ed16
Gal Passi <galpassi+1@gmail.com>
2025-06-19 13:04:32 +0300

    ESM SETUP

--- utils.py
+++ utils.py
@@ -13,3 +13,13 @@ def process_raw_variant(description: str):
         if not res:
             raise ValueError("Invalid input valid format of form p.{AA}{location}{AA}")
         return res.group('orig'), res.group('change'), int(res.group('location'))
+
+def esm_setup(model_name=ESM1B_MODEL):
+    """
+    :param model_name: str model name
+    :return: model, alphabet api
+    """
+    model, alphabet = torch.hub.load("facebookresearch/esm:main", model_name)
+    model = model.to(DEVICE)
+    print(f"model loaded on {DEVICE}")
+    return model, alphabet
